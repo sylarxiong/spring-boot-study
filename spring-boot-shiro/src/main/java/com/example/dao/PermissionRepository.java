44cad2b92c1f48f0cfd74e24b4203e7e3109bf30
xiongjingui <527008365@qq.com>
2019-01-02 16:30:12 +0800

    delete

--- spring-boot-shiro/src/main/java/com/example/dao/PermissionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.dao;
-
-import com.example.entity.Permission;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PermissionRepository extends JpaRepository<Permission,Integer> {
-}
--- spring-boot-shiro/src/main/java/com/example/service/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.example.service;
-
-import com.example.entity.User;
-
-public interface UserService {
-    User getUserByUsername(String username);
-}
