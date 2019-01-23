b23fbebdd17623bffe8b349a7232feebdd4f75f3
xiongjingui <527008365@qq.com>
2019-01-23 18:58:50 +0800

    s

--- spring-boot-basewebapp/src/main/java/com/demo/Application.java
+++ spring-boot-basewebapp/src/main/java/com/demo/Application.java
@@ -3,7 +3,7 @@ package com.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.demo")
 public class Application {
 
     public static void main(String[] args) {
