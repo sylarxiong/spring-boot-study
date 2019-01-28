1d62c214632705a8d636fc51d14f161d9cd3143c
xiongjingui <527008365@qq.com>
2019-01-28 10:51:32 +0800

    。

--- spring-boot-basewebapp/src/main/java/com/demo/controller/MainController.java
+++ spring-boot-basewebapp/src/main/java/com/demo/controller/MainController.java
@@ -4,11 +4,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class MainController {
 
-  @RequestMapping("/")
-  public String index() {
-    return "hello world!";
-  }
+    @RequestMapping("/hello")
+    public String hello() {
+        return "hello world!";
+    }
+
+    @RequestMapping("/login")
+    public String login() {
+        return "welcome!";
+    }
+
+    @RequestMapping("/info")
+    public String info() {
+        return "admin!";
+    }
 
 }
