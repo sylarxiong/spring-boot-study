41aa0d2b71d85b0b460dcacf22f5286d8a865a58
xiongjg <>
2020-03-27 07:59:37 +0800

    up

--- spring-boot-elasticsearch/src/main/java/com/example/controller/Book2Controller.java
+++ spring-boot-elasticsearch/src/main/java/com/example/controller/Book2Controller.java
@@ -1,16 +1,35 @@
 package com.example.controller;
 
+import com.example.common.NormalException;
 import com.example.common.Result;
 import com.example.entity.Book;
+import io.swagger.annotations.ApiOperation;
+import org.apache.commons.lang3.StringUtils;
+import org.elasticsearch.action.*;
+import org.elasticsearch.action.delete.DeleteRequestBuilder;
+import org.elasticsearch.action.delete.DeleteResponse;
+import org.elasticsearch.action.search.SearchRequestBuilder;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.action.search.SearchType;
+import org.elasticsearch.action.update.UpdateRequest;
+import org.elasticsearch.action.update.UpdateRequestBuilder;
+import org.elasticsearch.client.ElasticsearchClient;
+import org.elasticsearch.common.xcontent.XContentBuilder;
+import org.elasticsearch.common.xcontent.XContentFactory;
+import org.elasticsearch.index.query.BoolQueryBuilder;
+import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.index.query.RangeQueryBuilder;
+import org.elasticsearch.threadpool.ThreadPool;
 import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
-import org.springframework.data.elasticsearch.core.query.GetQuery;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.elasticsearch.core.query.*;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
-import javax.management.Query;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/template")
@@ -28,84 +47,46 @@ public class Book2Controller {
 
     @GetMapping("/all")
     public Result getAll() {
-        //Client client = elasticsearchTemplate.getClient();
-        //SearchRequestBuilder srb = client.prepareSearch("book").setTypes("novel");
-        //SearchResponse sr = srb.setQuery(QueryBuilders.matchAllQuery()).execute().actionGet(); // 查询所有
-        /*SearchRequestBuilder searchRequestBuilder = elasticsearchTemplate.getClient()
-                .prepareSearch("book")
-                .setTypes("novel")
-                .setQuery(QueryBuilders.matchAllQuery());
-        SearchResponse sr = searchRequestBuilder.get();
-        SearchHits hits = sr.getHits();
-        List<Map<String, Object>> list = new ArrayList<>();
-        hits.forEach(item -> list.add(item.getSourceAsMap()));*/
-       /* SearchQuery searchQuery = QueryBuilders.matchAllQuery();
+//        CriteriaQuery criteriaQuery = new CriteriaQuery(new Criteria());
+//        List<Book> list = elasticsearchOperations.queryForList(criteriaQuery,Book.class);
 
-        elasticsearchOperations.queryForList();*/
-
-        return Result.builder().success().message("查询成功").build();
+        SearchQuery searchQuery = new NativeSearchQueryBuilder().build();
+        List<Book> list = elasticsearchOperations.queryForList(searchQuery, Book.class);
+        return Result.builder().success().message("查询成功").data(list).build();
     }
-/*
+
     @PostMapping("/add")
     public Result add(@RequestBody Book book) {
-        try {
-            XContentBuilder xContentBuilder = XContentFactory.jsonBuilder()
-                    .startObject()
-                    .field("author", book.author)
-                    .field("bookName", book.bookName)
-                    .field("press", book.press)
-                    //注意这个时间 一定要加上.getTime() 否则会报错
-                    .field("publishedDate", book.publishedDate.getTime())
-                    .endObject();
-            elasticsearchTemplate.getClient().prepareIndex("book", "novel").setSource(xContentBuilder).get();
-            return Result.builder().success().message("保存成功").build();
-        } catch (IOException e) {
-            throw new NormalException(e.getMessage());
-        }
+        IndexQuery indexQuery = new IndexQueryBuilder().withObject(book).withIndexName("book").withType("novel").build();
+        elasticsearchOperations.index(indexQuery);
+        return Result.builder().success().message("添加成功").build();
     }
 
     @DeleteMapping("/delete")
     public Result delete(@RequestParam String id) {
-        DeleteResponse deleteResponse = elasticsearchTemplate.getClient().prepareDelete("book", "novel", id).get();
-        return Result.builder().success().message("删除成功").data(deleteResponse).build();
+        elasticsearchOperations.delete(Book.class, id);
+        return Result.builder().success().message("删除成功").build();
     }
 
     @PutMapping("/update")
     public Result update(@RequestBody Book book) {
         UpdateRequest updateRequest = new UpdateRequest("book", "novel", book.getId());
-        try {
-            XContentBuilder xContentBuilder = XContentFactory.jsonBuilder()
-                    .startObject();
-            if (StringUtils.isNoneBlank(book.getAuthor())) {
-                xContentBuilder.field("author", book.author);
-            }
-            if (StringUtils.isNoneBlank(book.getBookName())) {
-                xContentBuilder.field("bookName", book.bookName);
-            }
-            if (StringUtils.isNoneBlank(book.getPress())) {
-                xContentBuilder.field("press", book.press);
-            }
-            if (book.getPublishedDate() != null) {
-                //注意这个时间 一定要加上.getTime() 否则会报错
-                xContentBuilder.field("publishedDate", book.publishedDate.getTime());
-            }
-            if (book.getPrice() != null) {
-                xContentBuilder.field("price", book.price.doubleValue());
-            }
-            xContentBuilder.endObject();
-            updateRequest.doc(xContentBuilder);
-        } catch (IOException e) {
-            throw new NormalException(e.getMessage());
-        }
-        try {
-            elasticsearchTemplate.getClient().update(updateRequest).get();
-            return Result.builder().success().message("更新成功").build();
-        } catch (Exception e) {
-            throw new NormalException(e.getMessage());
-        }
+        UpdateQuery updateQuery = new UpdateQueryBuilder()
+                .withId(book.getId())
+                .withIndexName("book")
+                .withType("novel")
+                .withClass(Book.class)
+                .withUpdateRequest(new UpdateRequestBuilder(u))
+                .build();
+
+        elasticsearchOperations.update(updateQuery);
+        IndexQuery indexQuery = new IndexQueryBuilder().withObject(book).withIndexName("book").withType("novel").build();
+        elasticsearchOperations.index(indexQuery);
+        return Result.builder().success().message("更新成功").build();
+
 
     }
-
+/*
     @ApiOperation("复合查询")
     @GetMapping("/query")
     public Result compoundQuery(@RequestParam(required = false) String bookName,
